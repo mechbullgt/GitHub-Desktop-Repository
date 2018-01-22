55d1470e7200a1cad332d684a3ef0c935faa56e3
Maqbool-PC\Maqbool <maqbool123ansari@gmail.com>
2018-01-22 11:09:33 +0530

    Fixed syntax errors and logic

--- HrR_Points_Comparision_Problem.cpp
+++ HrR_Points_Comparision_Problem.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 vector < int > solve(int a0, int a1, int a2, int b0, int b1, int b2){
-    int scorea,scoreb=0;
+    int scorea =0;
+    int scoreb=0;
     vector<int> arr(6);
     arr={a0,a1,a2,b0,b1,b2};
     
@@ -12,23 +13,24 @@ vector < int > solve(int a0, int a1, int a2, int b0, int b1, int b2){
     //    arr.push_back(a2);
     //    arr.push_back(b0);arr.push_back(b1);arr.push_back(b2);
     
-    for (int i=0,j=3;i<3,j<6;i++,j++)
+    for (int i=0;i<3;i++)
     {
-        if(arr[i]>arr[j])
+        if(arr[i]>arr[i+3])
         {
             scorea = scorea+1;
         }
-        else if( arr[i]<arr[j])
+        else if( arr[i]<arr[i+3])
         {
             scoreb = scoreb+1;
         }
         else
         {
             scorea= scorea+0;
-            scoreb=scoreb+0;
+            scoreb= scoreb+0;
         }
     }
-    vector<int> s(scorea,scoreb);
+    vector<int> s(2);
+    s={scorea, scoreb};
     
     return s;
 }
